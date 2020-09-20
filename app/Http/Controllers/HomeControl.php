bfbbee46b34d870f06afa5c41b98ff31c857c338
Nasihjaseem <nasihjaseem88@gmail.com>
2020-09-20 12:15:48 +0530

    FirstCommit

---
+++ app/Http/Controllers/HomeControl.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace app\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeControl extends Controller
+{
+    public function index(){
+        // return view('welcome');
+        echo "Blabla";
+    }
+}
+
